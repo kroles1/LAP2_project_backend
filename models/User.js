fb1c1202370940b5bfbb547a69545f84bad778c0
kroles1 <korneliaoles@gmail.com>
2022-10-24 15:45:41 +0100

    auth backend

--- models/User.js
+++ models/User.js
@@ -63,6 +63,19 @@ class User {
     //     });
     // }
 
+    static findByEmail(email){
+        return new Promise(async (res, rej) => {
+            try {
+                let result = await db.query(`SELECT * FROM users
+                                                WHERE email = $1;`, [email]);
+                let user = new User(result.rows[0])
+                res(user)
+            } catch (err) {
+                rej(`Error retrieving user: ${err.message}`)
+            }
+        })
+    }
+
 }
 
 module.exports = User
