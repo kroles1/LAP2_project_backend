cf3d76317471cf937ac561f0310ade2a40c4e293
Sean <millington.sean12@gmail.com>
2022-10-24 16:51:06 +0200

    controllers almost done

--- controllers/users.js
+++ controllers/users.js
@@ -2,31 +2,41 @@ const User = require('../models/User')
 
 async function index (req, res) {
     try {
-
+        const users = await User.all
+        res.status(200).json(users)
     } catch (err) {
-
+        res.status(500).send({err})
     }
 }
 
 async function getById (req, res) {
     try {
-
+        const user = await User.getById(req.params.id)
+        res.status(200).json(user)
     } catch (err) {
-        
+        res.status(404).json({err})
     }
 }
 
 async function create (req, res) {
     try {
-
+        const user = await User.create(req.body)
+        res.status(201).json(user)
     } catch (err) {
-        
+        res.status(422).json({err})
     }
 }
 
 async function edit (req, res) {
     try {
-        // fetch habit data
+        const user = await User.edit(req.body)
+        res.status(201).json(user)
+    } catch (err) {
+        res.status(422).json({err})
+    }
+}
+
+// this might need to go in the font end
         // check habit difficulty
         // if diff = easy, exp = 10 // medium, exp = 20
         // set variable "increase" to 10/20/..
@@ -34,16 +44,7 @@ async function edit (req, res) {
         // calculate difference to next level
         // if (User.exp +  increase)  > exp required for next level    ->   increase =  remaining exp after leveling , level + 1
         // User.edit(increase, levelup (bool))
-    } catch (err) {
-        
-    }
-}
 
-// async function destroy (req, res) {
-//     try {
 
-//     } catch (err) {
-        
-//     }
-// }
+
 module.exports = {index, create, getById, edit}
