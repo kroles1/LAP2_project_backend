a0edc5719c3e7302565ff17c07d2498519c87fd7
Tca0 <taher.khtieb@gmail.com>
2022-10-24 20:07:07 +0100

    user edit habit route functuinalty

--- routes/habits.js
+++ routes/habits.js
@@ -7,6 +7,8 @@ router.get('/', authN, habitController.index) // get all habits for the user
 router.get('/:id', authN, habitController.getById) // used when user interacts with habit
 router.post('/', authN ,habitController.create) // create new habit
 router.patch('/:id', habitController.edit) // edit existing habit
+// this router with put method
+router.put('/:id', habitController.edit)   
 router.delete('/:id', habitController.destroy) // delete habit
 
 module.exports = router
