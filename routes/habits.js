361088ed9aa1f72dae61cc76945ed8fa2292e499
Tca0 <taher.khtieb@gmail.com>
2022-10-24 18:40:16 +0100

    adding authentication to get a habit by id

--- routes/habits.js
+++ routes/habits.js
@@ -4,7 +4,7 @@ const habitController = require('../controllers/habits')
 const authN = require('../middleware/authN')
 
 router.get('/', authN, habitController.index) // get all habits for the user
-router.get('/:id', habitController.getById) // used when user interacts with habit
+router.get('/:id', authN, habitController.getById) // used when user interacts with habit
 router.post('/', authN ,habitController.create) // create new habit
 router.patch('/:id', habitController.edit) // edit existing habit
 router.delete('/:id', habitController.destroy) // delete habit
