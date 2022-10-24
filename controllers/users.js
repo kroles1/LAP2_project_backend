deaeaded5ef2a40f2254b6026e5b4ac0f74fca1e
Sean <millington.sean12@gmail.com>
2022-10-24 13:32:57 +0200

    added user model

--- controllers/users.js
+++ controllers/users.js
@@ -26,7 +26,14 @@ async function create (req, res) {
 
 async function edit (req, res) {
     try {
-
+        // fetch habit data
+        // check habit difficulty
+        // if diff = easy, exp = 10 // medium, exp = 20
+        // set variable "increase" to 10/20/..
+        // check how close to next level
+        // calculate difference to next level
+        // if (User.exp +  increase)  > exp required for next level    ->   increase =  remaining exp after leveling , level + 1
+        // User.edit(increase, levelup (bool))
     } catch (err) {
         
     }
