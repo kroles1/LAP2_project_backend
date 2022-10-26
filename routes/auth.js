33786d71b7570633ff6a04835bb07f293959d9b0
Tca0 <taher.khtieb@gmail.com>
2022-10-26 18:14:37 +0100

    handelling duplicated key on db

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth')
 
-router.post('/', authController.register) // registration
-router.get('/', authController.login) // login
+router.post('/', authController.register)
+router.get('/', authController.login)
 
 
 module.exports = router
