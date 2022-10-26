c8bb7d605aa763d1cd40370439ee94b294eee2b5
Tca0 <taher.khtieb@gmail.com>
2022-10-26 12:47:50 +0100

    changing models bug names

--- middleware/authN.js
+++ middleware/authN.js
@@ -1,6 +1,6 @@
 // this is authentication process to decide if user have a valid token or not
 const jwt = require("jsonwebtoken");
-const User = require('../Models/User')
+const User = require('../models/User')
 const dotenv = require("dotenv");
 dotenv.config();
 
